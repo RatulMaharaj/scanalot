ef394432a1d3bf16552253c2a67e0af6353a33cf
Ratul Maharaj <ratulmaharaj@gmail.com>
2021-09-19 15:01:37 +0200

    Use for iPhone 12

---
+++ iStore.py
@@ -0,0 +1,27 @@
+from utils.email import send_email, checkIfSent
+from bs4 import BeautifulSoup
+import requests
+
+store_name = "iStore"
+
+if checkIfSent(store_name) is False:
+    notify = False
+    inStock = ""
+
+    link = "https://www.istore.co.za/iphone-12-pro"
+
+    r = requests.get(link)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    try:
+        inStock = soup.find(id="subtext-404").get_text().strip().lower()
+    except:
+        pass  # do nothing if the item doesn't exist
+
+    if inStock != "the page you are looking for cannot be found.":
+        notify = True
+
+    print(f"{store_name} has stock: ", notify)
+
+    if notify:  # Send a notification
+        send_email(store_name, link)
--- utils/email.py
+++ utils/email.py
@@ -4,11 +4,11 @@ from sendgrid.helpers.mail import Mail
 import json
 
 
-def send_email(company, link):
+def send_email(company, link, product="PS5"):
     message = Mail(
         from_email=os.environ.get("FROM_EMAIL"),
-        to_emails=os.environ.get("TO_EMAIL").split(),
-        subject=f"The PS5 is available at {company}!",
+        to_emails=os.environ.get("TO_EMAIL"),
+        subject=f"The {product} is available at {company}!",
         html_content=f"Click on this <a href='{link}'>link</a> to buy!",
     )
     try:
